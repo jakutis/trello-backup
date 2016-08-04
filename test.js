9113a947a994cddb94b7f6444a1d1486ada4615c
Vytautas Jakutis <vytautas@jakut.is>
2016-08-04 19:50:23 +0300

    resolve #2 Cannot handle boards with slashes in names

--- test.js
+++ test.js
@@ -8,6 +8,29 @@ var fs = require('./lib/fs');
 var trello = require('./lib/trello');
 
 var tests = [
+  function boardNameWithASlash() {
+    sinon.stub(fs, 'readJSONFile', _.constant({ backupDirectory: 'backups' }));
+    sinon.stub(trello, 'getBoards', _.constant(Promise.resolve({ boardId: 'board/name' })));
+    sinon.stub(trello, 'downloadBoard', _.noop);
+    sinon.stub(trello, 'downloadAttachments', _.noop);
+    sinon.stub(console, 'log', _.noop);
+    function restore() {
+      fs.readJSONFile.restore();
+      trello.getBoards.restore();
+      trello.downloadBoard.restore();
+      trello.downloadAttachments.restore();
+      console.log.restore();
+    }
+
+    return trelloBackup('config')
+      .then(function () {
+        assert.equal(
+          trello.downloadBoard.getCall(0).args[0].filename,
+          'backups/boards/boardId - board_name.json'
+        );
+      })
+      .finally(restore);
+  }
 ];
 
 Promise
